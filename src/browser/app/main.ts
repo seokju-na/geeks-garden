dad8a8879e40695d8d962a6c1414f43cd1376198
seokju-na <seokju.me@gmail.com>
2019-09-07 14:16:34 +0900

    Move 'browser/app' files

--- src/browser/app/main.ts
+++ src/browser/app/main.ts
@@ -1,7 +1,7 @@
 import { enableProdMode } from '@angular/core';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
-import { environment } from '../core/environment';
-import { AppModule } from './app/app-module';
+import { environment } from '../../core/environment';
+import { AppModule } from './app-module';
 
 if (environment.production) {
   enableProdMode();
