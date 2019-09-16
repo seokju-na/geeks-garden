5494efc38eec46aa4559727233ef29dd3508b317
seokju-na <seokju.me@gmail.com>
2019-09-16 22:51:29 +0900

    Read theme info from storage

--- src/browser/shared/shared-module.ts
+++ src/browser/shared/shared-module.ts
@@ -1,6 +1,5 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
-import { ThemeDatabaseProvider } from './databases/theme-database';
 import { HiddenHeading } from './directives/hidden-heading';
 import { ApiService } from './services/api-service';
 import { ThemeService } from './services/theme-service';
@@ -10,7 +9,6 @@ import { ThemeService } from './services/theme-service';
     CommonModule,
   ],
   providers: [
-    ThemeDatabaseProvider,
     ThemeService,
     ApiService,
   ],
