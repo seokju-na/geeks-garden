b3a67070448e2a1ca1d1a77afe6c38e9a1f5de39
seokju-na <seokju.me@gmail.com>
2019-09-17 01:43:17 +0900

    Add app packaging system (#4)
    * Add dependencies
    
    * Add 'electron-builder'. Remove 'devtron'
    
    * Add package script
    
    * Remove devtron
    
    * Ignore 'build/' directory

--- src/core/preload.js
+++ src/core/preload.js
@@ -1,10 +1,5 @@
 const { ipcRenderer, remote } = require('electron');
 
-if (process.env.NODE_ENV !== 'production') {
-  window.__devtron = { require: require, process: process };
-  require('devtron').install();
-}
-
 /**
  * Since we disable node integration for browser window, require electron
  * features at preload.
