a7b0c3b56bb27ef9b8cf805fe8bf922c09f57e83
seokju-na <seokju.me@gmail.com>
2019-09-19 00:22:32 +0900

    Add form control integrate with theme changes

--- src/browser/onboarding/components/welcome/welcome.ts
+++ src/browser/onboarding/components/welcome/welcome.ts
@@ -1,10 +1,37 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { FormControl } from '@angular/forms';
+import { Subscription } from 'rxjs';
+import { ThemeService } from '../../../shared/services/theme-service';
+import { Themes } from '../../../ui/style/theme';
 
 @Component({
   selector: 'gg-welcome',
   templateUrl: './welcome.html',
   styleUrls: ['./welcome.scss'],
 })
-export class Welcome {
-  readonly headingId = 'gg-welcome-heading';
+export class Welcome implements OnInit, OnDestroy {
+  readonly themeFormValue = new FormControl(
+    this.themeService.currentTheme,
+  );
+
+  readonly themeOptions = [
+    { value: Themes.BASIC_LIGHT_THEME, icon: 'sun-fill' },
+    { value: Themes.BASIC_DARK_THEME, icon: 'moon-fill' },
+  ];
+
+  private themeFormValueChangesSubscription = Subscription.EMPTY;
+
+  constructor(private readonly themeService: ThemeService) {
+  }
+
+  ngOnInit(): void {
+    this.themeFormValueChangesSubscription =
+      this.themeFormValue.valueChanges.subscribe((value) => {
+        this.themeService.setTheme(value as Themes);
+      });
+  }
+
+  ngOnDestroy(): void {
+    this.themeFormValueChangesSubscription.unsubscribe();
+  }
 }
